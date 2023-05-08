a777b856cd2889456f49fb9af34a86e3c6070aad
hsinx <cc20021111@gmail.com>
2023-05-08 11:53:01 +0800

    Modify Menu

--- app/src/main/java/fcu/app/myapplication/Menu_1Activity.java
+++ app/src/main/java/fcu/app/myapplication/Menu_1Activity.java
@@ -3,12 +3,14 @@ package fcu.app.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
+public class Menu_1Activity extends AppCompatActivity {
+  private Button btnSignin;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_menu);
+    setContentView(R.layout.activity_menu_1);
   }
 }
