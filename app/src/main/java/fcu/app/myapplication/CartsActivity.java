a92a3f687be217cdabd045405a61fe847c0a3846
wei <marvelous777777@gmail.com>
2023-05-10 20:13:35 +0800

    home_layout

--- app/src/main/java/fcu/app/myapplication/CartsActivity.java
+++ app/src/main/java/fcu/app/myapplication/CartsActivity.java
@@ -1,4 +1,14 @@
 package fcu.app.myapplication;
 
-public class CartsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carts);
+    }
 }
